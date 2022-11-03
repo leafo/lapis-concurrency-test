4afa6a88a70aa8e555dded5830331c6ccb283190
leaf corcoran <leafot@gmail.com>
2022-11-03 01:17:45 -0700

    add nginx config variant

--- app.lua
+++ app.lua
@@ -3,10 +3,17 @@ local app = lapis.Application()
 
 local cqueues = require "cqueues"
 
+local function sleep(delay)
+  if ngx then
+    return ngx.sleep(delay)
+  else
+    return cqueues.sleep(delay)
+  end
+end
+
 local db = require "lapis.db"
 
 app:get("/", function(self)
-  -- local res = db.query("select pg_sleep(2), * from pg_stat_activity")
   return self:html(function()
     h1 "Concurrent request test"
 
@@ -18,7 +25,8 @@ app:get("/", function(self)
           width = 200,
           height = 200,
           src = self:url_for("sub_request", nil, {
-            request_number = i
+            request_number = i,
+            delay_type = "sleep",
           })
         }
       end
@@ -29,13 +37,22 @@ end)
 app:get("sub_request", "/sub-req", function(self)
   local start_time = cqueues.monotime()
   local delay = math.random()*2 + 0.01
-  local res = nil
-  -- cqueues.sleep(delay)
-  local res = db.query("select pg_sleep(?), ? as request_number; select * from pg_stat_activity", delay, self.params.request_number)
+  local res
+  local sleep_type = self.params.delay_type or "query"
+
+  if sleep_type == "sleep" then
+    res = sleep(delay)
+  elseif sleep_type == "query" then
+    res = db.query("select pg_sleep(?), ? as request_number; select * from pg_stat_activity", delay, self.params.request_number)
+  end
+
+
   return self:html(function()
     div(function()
       strong("Request ".. self.params.request_number)
-      text(" Got back " .. res[1][1].request_number)
+      if res and res[1] and res[1][1] then
+        text(" Got back " .. res[1][1].request_number)
+      end
     end)
     div("Elapsed: " .. ("%0.3f"):format(cqueues.monotime() - start_time))
     div("Delay: " .. ("%0.3f"):format(delay))
--- config.lua
+++ config.lua
@@ -1,4 +1,5 @@
-require "lapis.config" ("development", {
+
+require "lapis.config" ("cqueues", {
   server = "cqueues",
   port = 8181,
   code_cache = true,
@@ -6,5 +7,17 @@ require "lapis.config" ("development", {
   postgres = {
     database = "postgres",
     socket_type = "cqueues",
+  }
+})
+
+
+require "lapis.config" ("nginx", {
+  server = "nginx",
+  port = 8181,
+  code_cache = "on",
+  measure_performance = true,
+  postgres = {
+    database = "postgres",
+    socket_type = "nginx",
   }
 })
